4c9eb10f029c55cf85ad77f56c6e604624654bda
Dan Shallal <dshallal11@gmail.com>
2024-09-28 16:23:06 -0400

    filtration

--- filtration.py
+++ filtration.py
@@ -4,7 +4,7 @@ import pandas as pd
 read_data = pd.read_csv("data.csv")
 
 read_data.drop(columns=["Footnote", "Data As Of", "Pneumonia Deaths", "Influenza Deaths", "Pneumonia or Influenza",
-                        "Pneumonia, Influenza, or COVID-19 Deaths"], inplace=True)
+                        "Pneumonia, Influenza, or COVID-19 Deaths", "Week Ending Date", "Indicator", "Group"], inplace=True)
 
 regional_data = read_data[read_data["Jurisdiction"].str.contains("HHS")]
 regional_data_final = regional_data[regional_data["Age Group"].str.contains("All Ages")]
@@ -27,7 +27,3 @@ for i in range(4):
     for j in range(1, 11):
         current = list_of_regions[i][list_of_regions[i]["Jurisdiction"].str.contains(r'\b' + "HHS Region " + str(j) + r'\b', regex=True, case=False)]
         list_of_regions2.append(current)
-
-
-
-print(list_of_regions2[10])
